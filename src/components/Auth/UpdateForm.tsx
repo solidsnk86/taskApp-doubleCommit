db2d997a91016df7dfcb64700a5b460024aa6fac
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-19 21:34:27 -0300

    styles

--- src/components/Auth/UpdateForm.tsx
+++ src/components/Auth/UpdateForm.tsx
@@ -258,7 +258,7 @@ export const UpdateForm = () => {
               <path d="M7 11V7a5 5 0 0 1 10 0v4" />
             </svg>
 
-            <p>Actualizar contraseña</p>
+            <p className="hover:underline hover:text-blue-500">Actualizar contraseña</p>
           </div>
           {/* Botones */}
           <aside className="flex justify-center gap-3 mt-4">
