98d5a1110f72e8b501e130e32d806d4527274861
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-22 17:02:27 -0300

    updated favicon and logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -12,9 +12,9 @@ export const Header = () => {
         <div
           title="Inicio"
           onClick={() => navigate("/")}
-          className="flex items-center gap-2 cursor-pointer hover:scale-105 transition-transform duration-500"
+          className="flex items-center gap-1 cursor-pointer hover:scale-105 transition-transform duration-500"
         >
-          <img src="/vite.svg" width={33} height={33} loading="eager" alt="Logo" />
+          <img src="/logo-doubleCommit-taskApp.png" width={33} height={33} loading="eager" alt="Logo" />
           <span className="font-semibold text-lg text-transparent bg-clip-text bg-gradient-to-r from-[#57B4FF] via-[#8D70FE] to-[#BD34FE] hidden md:flex">
             doubleCommit taskApp
           </span>
